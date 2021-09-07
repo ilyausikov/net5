13019f26038312f474d4e4fd7be6ef15c4908354
ilyausikov <ilyausikov@coherentsolutions.com>
2021-09-07 14:25:54 +0300

    * changed pre-build

--- net5/net5.iOS/appcenter-pre-build.sh
+++ net5/net5.iOS/appcenter-pre-build.sh
@@ -1,19 +1,10 @@
 #!/usr/bin/env bash
 
-echo "**************************************************************************************************"
-echo "iOS Pre Build Script"
-echo "**************************************************************************************************"
-
-# Download Mono 6.12.0.112
-wget https://download.mono-project.com/archive/6.12.0/macos-10-universal/MonoFramework-MDK-6.12.0.122.macos10.xamarin.universal.pkg
+# Download Mono 6.12.0.145
+wget https://download.mono-project.com/archive/6.12.0/macos-10-universal/MonoFramework-MDK-6.12.0.145.macos10.xamarin.universal.pkg
 
 # Add execution permission
-sudo chmod +x MonoFramework-MDK-6.12.0.122.macos10.xamarin.universal.pkg
+sudo chmod +x MonoFramework-MDK-6.12.0.145.macos10.xamarin.universal.pkg
 
 # Install Mono 6.12.0.145
-sudo installer -pkg MonoFramework-MDK-6.12.0.122.macos10.xamarin.universal.pkg -target /
-
-echo ""
-echo "**************************************************************************************************"
-echo "iOS Pre Build Script complete"
-echo "**************************************************************************************************"
+sudo installer -pkg MonoFramework-MDK-6.12.0.145.macos10.xamarin.universal.pkg -target /
