8966a104a5242a82f9d4ceceb8d37bc7df71ef18
ilyausikov <ilyausikov@coherentsolutions.com>
2021-09-07 10:22:52 +0300

    * initial commit

---
+++ net5/net5/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Xunit;
+
+namespace net5
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.True(true);
+        }
+    }
+}
