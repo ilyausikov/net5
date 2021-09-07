6c46c92a15a4467efb65d5e226b883dd9ad38034
ilyausikov <ilyausikov@coherentsolutions.com>
2021-09-07 11:22:32 +0300

    * added logs to pre-build script

--- net5/net5.iOS/appcenter-pre-build.sh
+++ net5/net5.iOS/appcenter-pre-build.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+echo "**************************************************************************************************"
+echo "iOS Pre Build Script"
+echo "**************************************************************************************************"
+
 # Download Mono 6.12.0.112
 wget https://download.mono-project.com/archive/6.12.0/macos-10-universal/MonoFramework-MDK-6.12.0.122.macos10.xamarin.universal.pkg
 
@@ -7,4 +11,9 @@ wget https://download.mono-project.com/archive/6.12.0/macos-10-universal/MonoFra
 sudo chmod +x MonoFramework-MDK-6.12.0.122.macos10.xamarin.universal.pkg
 
 # Install Mono 6.12.0.145
-sudo installer -pkg MonoFramework-MDK-6.12.0.122.macos10.xamarin.universal.pkg -target /  
+sudo installer -pkg MonoFramework-MDK-6.12.0.122.macos10.xamarin.universal.pkg -target /
+
+echo ""
+echo "**************************************************************************************************"
+echo "iOS Pre Build Script complete"
+echo "**************************************************************************************************"
