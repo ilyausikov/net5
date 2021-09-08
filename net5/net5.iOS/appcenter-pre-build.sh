1c510fd3bca7102032a9f1a88c01c66f144b4eec
ilyausikov <ilyausikov@coherentsolutions.com>
2021-09-08 16:44:05 +0300

    * extended pre build

--- net5/net5.iOS/appcenter-pre-build.sh
+++ net5/net5.iOS/appcenter-pre-build.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env bash
 
+# Pre Build Script
+
+set -e # Exit immediately if a command exits with a non-zero status (failure)
+
+echo "**************************************************************************************************"
+echo "iOS Pre Build Script"
+echo "**************************************************************************************************"
+
 export DOTNET_SKIP_FIRST_TIME_EXPERIENCE=true
 
 # Install preview version of Mono for C# 9 compat
