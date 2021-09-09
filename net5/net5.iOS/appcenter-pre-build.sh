a566c10580a66467e9b518177e3a4524e3611f64
ilyausikov <ilyausikov@coherentsolutions.com>
2021-09-09 21:59:49 +0300

    * turned off mono preview

--- net5/net5.iOS/appcenter-pre-build.sh
+++ net5/net5.iOS/appcenter-pre-build.sh
@@ -12,6 +12,6 @@ export DOTNET_SKIP_FIRST_TIME_EXPERIENCE=true
 
 # Install preview version of Mono for C# 9 compat
 dotnet tool install --global boots --version 1.0.4.624
-boots --preview Mono
+#boots --preview Mono
 #boots --preview XamarinAndroid
 #boots --preview XamariniOS
