13387446aadc8d4e458458dec049050700b6b03a
ilyausikov <ilyausikov@coherentsolutions.com>
2021-09-09 21:43:11 +0300

    * turned off xamarin android boots

--- net5/net5.iOS/appcenter-pre-build.sh
+++ net5/net5.iOS/appcenter-pre-build.sh
@@ -13,5 +13,5 @@ export DOTNET_SKIP_FIRST_TIME_EXPERIENCE=true
 # Install preview version of Mono for C# 9 compat
 dotnet tool install --global boots --version 1.0.4.624
 boots --preview Mono
-boots --preview XamarinAndroid
+#boots --preview XamarinAndroid
 boots --preview XamariniOS
